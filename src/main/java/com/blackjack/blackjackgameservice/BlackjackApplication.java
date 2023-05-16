c8edc416ffa5f18f212ec2ecb51ae1b105f673d0
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2023-05-16 20:44:33 -0300

    feat: implementing main class of Blackjack

--- src/main/java/com/blackjack/blackjackgameservice/BlackjackApplication.java
+++ src/main/java/com/blackjack/blackjackgameservice/BlackjackApplication.java
@@ -1,5 +1,7 @@
 package com.blackjack.blackjackgameservice;
 
+import com.blackjack.blackjackgameservice.services.DeckService;
+import com.blackjack.blackjackgameservice.services.PlayerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlackjackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlackjackApplication.class, args);
-	}
 
+		SpringApplication.run(BlackjackApplication.class, args);
+
+		DeckService deck = new DeckService();
+		deck.shuffle();
+
+		PlayerService player = new PlayerService();
+		player.addCard(deck.drawCard());
+		player.addCard(deck.drawCard());
+
+		System.out.println("Players hand:");
+		player.printHand();
+		System.out.println("Total point: " + player.getPoints());
+
+		if (player.isBust()) {
+			System.out.println("Bust! Player loses.");
+		} else if (player.getPoints() == 21) {
+			System.out.println("Blackjack 21! Player wins.");
+		} else {
+			System.out.println("Keep playing...");
+		}
+	}
 }
